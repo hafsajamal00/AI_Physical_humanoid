6c14078893a5abd14ec86b9053b1312da56b4053
hafsajamal00 <hafsajamal60@gmail.com>
2025-12-30 03:19:06 +0500

    Solved vercel error

--- book_frontend/docusaurus.config.js
+++ book_frontend/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://laiba.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI_Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
