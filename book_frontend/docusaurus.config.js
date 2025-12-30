a56e5718a8cfe8164202bcc437512db1dcc9ab32
hafsajamal00 <160578144+hafsajamal00@users.noreply.github.com>
2025-12-30 17:45:04 +0500

    Update docusaurus.config.js

--- book_frontend/docusaurus.config.js
+++ book_frontend/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://laiba.github.io',
+  url: 'https://aiphysicalhumanoid.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
